39b1b002b9189189e9cfa829f9ff9fd08a8cfc24
JeffersonDevR <jeffers1917@hotmail.com>
2025-09-30 16:19:23 -0500

    Agregando readme y estructura de archivos

---
+++ Modulos/Pagos.py
@@ -0,0 +1,17 @@
+#Procesamiento de pagos de manera asincrona, validando medios de pago, estado de las transacciones y confirmaciones automaticas
+
+
+class Pagos():
+    
+    def __init__(self):
+        self.medios_de_pago = ["Efectivo", "Tarjeta","Nequi"]
+        
+    def validar_pago():
+        pass
+        
+    def procesar_pago(self,medio_de_pago):
+        pass
+    
+    def mostrar_estados_transacciones(self):
+        pass        
+            
