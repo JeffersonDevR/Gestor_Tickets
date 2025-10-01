a965fbdfa8f9f7216cb4d7674f03fdf5581dfc58
JeffersonDevR <jeffers1917@hotmail.com>
2025-10-01 13:09:08 -0500

    Prueba

--- Modulos/Pagos.py
+++ Modulos/Pagos.py
@@ -6,12 +6,18 @@ class Pagos():
     def __init__(self):
         self.medios_de_pago = ["Efectivo", "Tarjeta","Nequi"]
         
-    def validar_pago():
-        pass
+    def validar_pago(self,metodo_pago_cliente):
+        if metodo_pago_cliente in self.medios_de_pago:
+            print("Pago procesado.")
+            
+        else:
+            print("Pago no procesado.")
         
     def procesar_pago(self,medio_de_pago):
         pass
     
     def mostrar_estados_transacciones(self):
         pass        
-            
+    
+    def procesar_reembolso(self,aprobado):
+        pass
