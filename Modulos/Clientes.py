ee0054c127219a9b99124e3f943c40b91c3a5f91
Cristian Camilo Gómez Parada <cristforx@gmail.com>
2025-10-02 02:24:28 -0500

    Creación de la estructura de la clase cliente, con la funcion de registrar cliente(funcion logica no texteada)

--- Modulos/Clientes.py
+++ Modulos/Clientes.py
@@ -1,1 +1,17 @@
-print("Prueba de Github")
+class Cliente:
+    def __init__(self,nombre,n_identificacion,correo):
+        self.nombre = nombre
+        self.n_identificacion = n_identificacion
+        self.correo = correo
+        self.historial_de_reservas = []
+        self.clientes_registrados = []
+
+    def registrar_cliente(self,cliente_nuevo):
+        for cliente_existente in self.clientes_registrados:
+            if cliente_existente.n_identificacion == cliente_nuevo.identificacion:
+                print("Este cliente ya se encuentra registrado en el sistema")
+                return
+        self.clientes_registrados.append(cliente_nuevo)
+            
+
+        pass
