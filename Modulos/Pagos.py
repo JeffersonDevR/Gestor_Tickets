9e3fba33754941128b5d08677fa3d614dd688d24
Jefferson Rodriguez <40008214+JeffersonDevR@users.noreply.github.com>
2025-10-02 17:14:52 +0000

    "Adiccion de diferentes classes segun los metodos de pagos"

--- Modulos/Pagos.py
+++ Modulos/Pagos.py
@@ -1,23 +1,63 @@
 #Procesamiento de pagos de manera asincrona, validando medios de pago, estado de las transacciones y confirmaciones automaticas
+from abc import ABC, abstractmethod
+from enum import Enum
+
+# class MetodoDePago(Enum):
+#     TARJETA_CREDITO = "tarjeta_credito"
+#     TRANSFERENCIA = "transferencia"
+#     EFECTIVO = "efectivo"
+
+class Pago(ABC):
+
+    def __init__(self,ammount:float,date):
+        self.ammount = ammount
+        self.date = date
+        self.status = "pending"
+
+    @abstractmethod
+    def process(self) -> bool:
+        pass
+
+    @abstractmethod
+    def validate(self) -> bool:
+        pass
 
 
-class Pagos():
-    
-    def __init__(self):
-        self.medios_de_pago = ["Efectivo", "Tarjeta","Nequi"]
+
+class PagoEnEfectivo(Pago):
+
+    def __init__(self, ammount, date):
+        super().__init__(ammount, date)
         
-    def validar_pago(self,metodo_pago_cliente):
-        if metodo_pago_cliente in self.medios_de_pago:
-            print("Pago procesado.")
-            
-        else:
-            print("Pago no procesado.")
+
+    def validate(self) -> bool:
+        return self.ammount > 0
+    
+    def process(self) -> bool;
+        if not self.validate():
+            return False
+        print(f"Pago en efectivo realizado: ${self.ammount}")
+        self.status = "completed" 
+        return True  
+
+class PagoConTarjeta(Pago):
+
+    def __init__(self, ammount, date,card_number:str,cvv:str):
+        super().__init__(ammount, date)
+        self.card_number - card_number
+        self.cvv = cvv
+
+    def validate(self) -> bool:
+
+        if len(self.card_number) != 26:
+            return False
+        if len(self.cvv) != 3:
+            return False
+        return True
         
-    def procesar_pago(self,medio_de_pago):
-        pass
-    
-    def mostrar_estados_transacciones(self):
-        pass        
-    
-    def procesar_reembolso(self,aprobado):
-        pass
+    def process(self) -> bool:
+        if not self.validate():
+            return False
+        print(f"Pago con tarjeta completado: ${self.amount}")
+        self.status = "completed"
+        return True
